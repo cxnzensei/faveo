59dfc5b854c9eb67407032e793c21992a703ebcd
cxnzensei <nishitpatil3231@gmail.com>
2023-05-09 05:37:15 +0530

    add experience

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
         </div>
         <Navbar />
         <About />
-        {/* <Experience /> */}
+        <Experience />
         {/* <Works /> */}
         {/* <Feedbacks /> */}
         <hr className="blur-sm" />
